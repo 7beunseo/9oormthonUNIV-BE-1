423836443752be6845dd9509ed0d0fcb7b5f0899
7beunseo <kimes0403@gmail.com>
2024-09-14 02:53:07 +0900

    ✨ [FEAT] 회원가입 API 작성 #1

--- src/main/java/goorm/backend/work1/config/SecurityConfig.java
+++ src/main/java/goorm/backend/work1/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package goorm.backend.work1.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Component
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers(
+                        "/user/signup"
+                ).permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
 }
